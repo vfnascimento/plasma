abdbfc6c352c9ee55b28f5c113db901d1191964b
Vinicius Nascimento <91392811+vfnascimento@users.noreply.github.com>
2024-07-11 14:59:39 -0300

    Update lambda_registro.py

---
+++ lambda/lambda_registro.py
@@ -0,0 +1,26 @@
+import json
+import boto3
+import os
+
+def lambda_handler(event, context):
+    # Parse the input JSON
+    data = json.loads(event['body'])
+    
+    # Extract the CPF to use as filename
+    cpf = data['cpf']
+    
+    # Create the S3 client
+    s3 = boto3.client('s3')
+    
+    # Define the bucket and file path
+    bucket_name = 'mh-cleaning'
+    file_path = f'user_data/{cpf}.json'
+    
+    # Save the data to S3
+    s3.put_object(Bucket=bucket_name, Key=file_path, Body=json.dumps(data))
+    
+    # Return a success response
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'message': 'Cadastro efetuado com sucesso'})
+    }
