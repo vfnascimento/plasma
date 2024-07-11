2cae93bbb6a8f42bb19a1cdca449c3226ab0acef
Vinicius Nascimento <91392811+vfnascimento@users.noreply.github.com>
2024-07-11 15:16:45 -0300

    Update userService.js

--- app/services/userService.js
+++ app/services/userService.js
@@ -1,5 +1,5 @@
 app.service('UserService', ['$http', function($http) {
     this.registerUser = function(user) {
-        return $http.post('https://seu-api-gateway-url/endpoint', user);
+        return $http.post('https://kztfp0t7ql.execute-api.us-east-1.amazonaws.com/stage/mh-cleaning-amplify-register', user);
     };
 }]);
