8de8ff4b901b988d4d31f0c6570e651765a12c5c
Vinicius Nascimento <91392811+vfnascimento@users.noreply.github.com>
2024-07-11 15:37:41 -0300

    Update loginService.js
    adicionado endpoint

--- app/services/loginService.js
+++ app/services/loginService.js
@@ -1,5 +1,5 @@
 app.service('LoginService', ['$http', function($http) {
     this.loginUser = function(loginData) {
-        return $http.post('https://seu-api-gateway-url/endpoint', loginData);
+        return $http.post('https://p722129cq0.execute-api.us-east-1.amazonaws.com/stage/mh-cleaning-amplify-login', loginData);
     };
 }]);
